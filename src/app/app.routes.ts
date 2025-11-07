4b6a8fecea263b3104be00f306f579c22501a99a
Tymofii Ivzhenko <tymofiiivzhenko@gmail.com>
2025-11-07 16:04:51 +0000

    Практична №1: базова структура, layout компонент

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
